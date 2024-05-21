b480b674432250a44cd77145e22de96e02c500d2
rufis <mateotemporelli97@gmail.com>
2024-05-21 20:27:36 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let iphone = prompt ("equipo que busca");
+
+console.log (iphone);
+
+let gb = prompt ("cantidad de gb que busca");
+
+console.log (gb);
+
+let color = prompt ("color que busca");
+
+console.log (color);
+
+function valorEnPesos (valorDelDolar, iphone13){
+
+    console.log(valorDelDolar * iphone13)
+}
+valorEnPesos(1230, 670);
+
+function valorEnPesos1 (valorDelDolar, iphone14){
+
+    console.log(valorDelDolar * iphone14)
+}
+valorEnPesos1(1230, 750);
+
+function valorEnPesos2 (valorDelDolar, iphone15){
+
+    console.log(valorDelDolar * iphone15)
+}
+valorEnPesos1(1230, 820);
+
+function valorEnPesos3 (valorDelDolar, iphone15Pro){
+
+    console.log(valorDelDolar * iphone15Pro)
+}
+valorEnPesos1(1230, 1050);
+
+function valorEnPesos4 (valorDelDolar, iphone15ProMax){
+
+    console.log(valorDelDolar * iphone15ProMax)
+}
+valorEnPesos1(1230, 1240);
